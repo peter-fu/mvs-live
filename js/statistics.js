a05023748ef799100f923391ba509f6e1bd34a4e
yangcongtou2016 <2297367632@qq.com>
2017-08-07 17:55:19 +0800

    yangcongtou

--- js/statistics.js
+++ js/statistics.js
@@ -1,22 +1,13 @@
 /*
- Growing IO统计代码
+ Growing IO
  statistics.js
 
  */
 
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?900a3bc4171911f3c362e041e2dd841d";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
-
-
 var _vds = _vds || [];
 window._vds = _vds;
 (function(){
-    _vds.push(['setAccountId', 'a2c97b45f75a0c37']);
+    _vds.push(['setAccountId', '906ebbbc3c25dc34']);
     (function() {
         var vds = document.createElement('script');
         vds.type='text/javascript';

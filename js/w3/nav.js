c9766a04575094503ec119f9498b29cc0e59e30b
yangcongtou2016 <2297367632@qq.com>
2017-11-22 18:13:39 +0800

    yct

--- js/w3/nav.js
+++ js/w3/nav.js
@@ -8,22 +8,22 @@ $(function(){
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
             if($(this).index()==0){
-                console.log('00');
+                // console.log('00');
                 $('html ,body').animate({scrollTop: 0}, 300);
             }else if($(this).index()==1){
-                console.log('11');
+                // console.log('11');
                 $('html ,body').animate({scrollTop: $('.box-01').height()}, 300);//2500
             }else if($(this).index()==2){
-                console.log('22');
+                // console.log('22');
                 //$('html ,body').animate({scrollTop: $('.box-01').height()+$('.box-02').height()}, 300);
             }else if($(this).index()==3){
-                console.log('33');
+                // console.log('33');
                 $('html ,body').animate({scrollTop: $('.box-01').height()+$('.box-02').height()+$('.box-03').height()}, 300);
             }else if($(this).index()==4){
-                console.log('44='+$('.box-01').height()+'|'+$('.box-02').height()+'|'+$('.box-03').height()+'|'+$('.box-04').height());
+                // console.log('44='+$('.box-01').height()+'|'+$('.box-02').height()+'|'+$('.box-03').height()+'|'+$('.box-04').height());
                 $('html ,body').animate({scrollTop: $('.box-01').height()+$('.box-02').height()+$('.box-03').height()+$('.box-04').height()}, 300);
             }else if($(this).index()==5){
-                console.log('55');
+                // console.log('55');
                 $('html ,body').animate({scrollTop: $('.box-01').height()+$('.box-02').height()}, 300);
             }
         });
